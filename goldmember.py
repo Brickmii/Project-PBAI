ceec822cfbdb2a917881de5e8b49db5d496da762
Brickmii <ianmcclenathan@gmail.com>
2026-01-14 15:55:19 -0500

    Delete goldmember.py

--- goldmember.py
+++
@@ -1,9 +0,0 @@
-# Goldmember diagnostic module
-# removable, included in base for debug,
-# designed to bootstrap api, golden loop, golden gate
-# Golden Gate - Registrar learning gate diagnosis
-# Golden Loop - Evaluator learning gate diagnosis
-# may be ran alongside tasks to assess learning
-# is not a standalone diagnosis program
-# assesses difference between information given and information learned
-# may be adjusted or added to for functional completion
